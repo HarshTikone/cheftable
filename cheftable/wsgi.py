e596ec8c85598f0b30eaf87be96820959b41afa4
Harshtikone <harshtikone028@gmail.com>
2023-06-16 16:00:12 +0530

    vercel added

--- cheftable/wsgi.py
+++ cheftable/wsgi.py
@@ -14,3 +14,5 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cheftable.settings')
 
 application = get_wsgi_application()
+
+app = application
